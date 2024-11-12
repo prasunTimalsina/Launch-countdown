3b1ca179f043f1d9548436832f6fe004f7b0888e
prasunTimalsina <timalsinaprasun61@gmail.com>
2024-11-12 08:41:16 +0545

    UI Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         redhat: ["Red Hat Text"],
       },
+      keyframes: {
+        flipDown: {
+          "100%": { transform: "rotateX(90deg)" },
+        },
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "flip-down": "flipDown 3s linear 1",
+      },
     },
   },
   plugins: [],
